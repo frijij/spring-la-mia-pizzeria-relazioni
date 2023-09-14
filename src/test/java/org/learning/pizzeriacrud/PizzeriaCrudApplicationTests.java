7341829b67cf4ae66a5040bb805e4f6011df9a9f
francesca <francipeco36@gmail.com>
2023-09-14 14:54:31 +0200

    file preparation

---
+++ src/test/java/org/learning/pizzeriacrud/PizzeriaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learning.pizzeriacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
