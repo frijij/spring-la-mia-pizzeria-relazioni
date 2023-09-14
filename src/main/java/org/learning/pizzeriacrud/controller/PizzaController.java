2eb79d165d56c73b4a308184e69dfc195720f295
francesca <francipeco36@gmail.com>
2023-09-14 16:28:46 +0200

    empty list message added

--- src/main/java/org/learning/pizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/learning/pizzeriacrud/controller/PizzaController.java
@@ -3,10 +3,12 @@ package org.learning.pizzeriacrud.controller;
 import org.learning.pizzeriacrud.model.Pizza;
 import org.learning.pizzeriacrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -20,6 +22,9 @@ public class PizzaController {
     @GetMapping
     public String index(Model model) {
         List<Pizza> listaPizze = repository.findAll();
+        if (listaPizze.size() == 0) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Non ci sono pizze disponibili al momento");
+        }
         model.addAttribute("pizze", listaPizze);
         return "index";
     }
