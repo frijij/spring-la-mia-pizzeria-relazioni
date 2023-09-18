e6f5aa01b08dc395a465716909d4604c358d7094
francesca <francipeco36@gmail.com>
2023-09-18 18:13:58 +0200

    optional method added

--- src/main/java/org/learning/pizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/learning/pizzeriacrud/controller/PizzaController.java
@@ -3,13 +3,16 @@ package org.learning.pizzeriacrud.controller;
 import org.learning.pizzeriacrud.model.Pizza;
 import org.learning.pizzeriacrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/pizze")
@@ -27,8 +30,13 @@ public class PizzaController {
 
     @GetMapping("/show/{pizzaId}")
     public String show(@PathVariable("pizzaId") Integer id, Model model) {
-        model.addAttribute("pizza", repository.findById(id).get());
-        return "/pizze/show";
+        Optional<Pizza> pizzaOptional = repository.findById(id);
+        if (pizzaOptional.isPresent()) {
+            model.addAttribute("pizza", pizzaOptional.get());
+            return "/pizze/show";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
