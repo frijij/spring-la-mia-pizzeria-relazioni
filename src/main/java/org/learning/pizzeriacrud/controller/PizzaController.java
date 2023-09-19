9af1afd1b50e49d2adf6a0e7ccd3cce64a106e95
francesca <francipeco36@gmail.com>
2023-09-19 16:03:44 +0200

    post method for create added

--- src/main/java/org/learning/pizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/learning/pizzeriacrud/controller/PizzaController.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -43,6 +41,11 @@ public class PizzaController {
     public String create(Model model) {
         model.addAttribute("pizza", new Pizza());
         return "/pizze/form";
+    }
+
+    @PostMapping("/create")
+    public String doCreate(@ModelAttribute("pizza") Pizza formPizza) {
+        return "redirect:/pizze";
     }
 
 }
