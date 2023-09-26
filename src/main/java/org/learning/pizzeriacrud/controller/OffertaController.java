347619a8c971ea2751d51caae7c7ba0ab710b557
francesca <francipeco36@gmail.com>
2023-09-26 16:40:28 +0200

    offerta layout in show + controller created

---
+++ src/main/java/org/learning/pizzeriacrud/controller/OffertaController.java
@@ -0,0 +1,46 @@
+package org.learning.pizzeriacrud.controller;
+
+import org.learning.pizzeriacrud.model.Offerta;
+import org.learning.pizzeriacrud.model.Pizza;
+import org.learning.pizzeriacrud.repository.OffertaRepository;
+import org.learning.pizzeriacrud.repository.PizzaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/offerte")
+public class OffertaController {
+    @Autowired
+    OffertaRepository offertaRepository;
+
+    @Autowired
+    PizzaRepository pizzaRepository;
+
+    @GetMapping("/create")
+    public String create(@RequestParam("pizzaId") Integer pizzaId, Model model) {
+        Optional<Pizza> pizzaResult = pizzaRepository.findById(pizzaId);
+        if (pizzaResult.isPresent()) {
+            Pizza pizza = pizzaResult.get();
+            Offerta offerta = new Offerta();
+            offerta.setPizza(pizza);
+            offerta.setDataInizio(LocalDate.now());
+            offerta.setDataFine(LocalDate.now());
+            model.addAttribute("offerta", offerta);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza " + pizzaId + " non trovata.");
+        }
+
+        return "/pizze/offerte/form";
+    }
+
+
+}
---
+++ src/main/java/org/learning/pizzeriacrud/repository/OffertaRepository.java
@@ -0,0 +1,7 @@
+package org.learning.pizzeriacrud.repository;
+
+import org.learning.pizzeriacrud.model.Offerta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OffertaRepository extends JpaRepository<Offerta, Integer> {
+}
