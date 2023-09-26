412c3c543a429323297796c04f4cd8fd5b303181
francesca <francipeco36@gmail.com>
2023-09-26 15:12:55 +0200

    attributes + getters&setters added to ordine

--- src/main/java/org/learning/pizzeriacrud/model/Ordine.java
+++ src/main/java/org/learning/pizzeriacrud/model/Ordine.java
@@ -1,4 +1,49 @@
 package org.learning.pizzeriacrud.model;
 
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "ordini")
 public class Ordine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private LocalDate dataInizio;
+    private LocalDate dataFine;
+    private String titolo;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getDataInizio() {
+        return dataInizio;
+    }
+
+    public void setDataInizio(LocalDate dataInizio) {
+        this.dataInizio = dataInizio;
+    }
+
+    public LocalDate getDataFine() {
+        return dataFine;
+    }
+
+    public void setDataFine(LocalDate dataFine) {
+        this.dataFine = dataFine;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
 }
