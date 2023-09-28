ddb30de4e3bbef8f90b6862c66615e6c4e6aae46
francesca <francipeco36@gmail.com>
2023-09-28 20:00:13 +0200

    show method done

--- src/main/java/org/learning/pizzeriacrud/controller/IngredienteController.java
+++ src/main/java/org/learning/pizzeriacrud/controller/IngredienteController.java
@@ -1,10 +1,32 @@
 package org.learning.pizzeriacrud.controller;
 
+import org.learning.pizzeriacrud.repository.IngredienteRepository;
+import org.learning.pizzeriacrud.repository.PizzaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/ingredienti")
 public class IngredienteController {
+    @Autowired
+    private IngredienteRepository ingredienteRepository;
+
+    @Autowired
+    private PizzaRepository pizzaRepository;
+
+    @GetMapping("/show")
+    public String show(Model model) {
+        model.addAttribute("ingredienti", ingredienteRepository.findAll());
+        return "/pizze/ingredienti/show";
+    }
+
+    @GetMapping("/create")
+    public String create() {
+
+        return "/pizze/ingredienti/form";
+    }
 
 }
